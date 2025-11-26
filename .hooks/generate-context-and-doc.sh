bba69f635da075a60d1e5aacacaa19ce252643fe
Jesper Friis <jesper.friis@sintef.no>
2025-11-26 15:57:27 +0100

    Generated context

--- .hooks/generate-context-and-doc.sh
+++ .hooks/generate-context-and-doc.sh
@@ -5,8 +5,8 @@ HERE="$(cd "$(dirname "$0")" && pwd)"
 cd "$HERE"/..
 
 # Generate JSON-LD context and keyword documentation
-keywords --yamlfile=context/0.1/keywords.yaml \
-         --context=context/0.1/context.json
+keywords --input=temgo/context/0.1/keywords.yaml \
+         --context=temgo/context/0.1/context.json
 
 
 # Don't crash pre-commit in case the above fails on GitHub
